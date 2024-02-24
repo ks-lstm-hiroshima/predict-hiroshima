cb74a2e00c5e28a5f2d002b5c2ed125053f4eaf8
tanaka <tanaka@knowlsat.jp>
2024-02-24 21:22:41 +0900

    batファイルを追加（ダブルクリックでプログラム起動）
    1. batフォルダを新規作成
    2. batフォルダ内に00分モデルと30分モデルのbatファイルを追加
    3. batフォルダ内のReadme.txtに従い、インストール先のフォルダを適宜設定すること

---
+++ PredictLSTM/bat/Predict_00.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+cd %~dp0
+
+set TestDir="C:\AllFiles\AI_Workspace\PredictLSTM"
+set Target=jp.knowlsat.lstm.LSTM_Ammonia_00
+
+cd %TestDir%
+
+javac -d %TestDir%\bin %TestDir%\src/jp/knowlsat/lstm/predict/*.java %TestDir%\src/jp/knowlsat/lstm/*.java
+java -cp %TestDir%\bin %Target%
+
+timeout /t 1500 > nul
---
+++ PredictLSTM/bat/ReadMe.txt
@@ -0,0 +1,7 @@
+各batファイルの5行目にあるPredictLSTMのインストール先のパスを適宜変更すること
+
+set TestDir="C:\AllFiles\AI_Workspace\PredictLSTM"
+
+↓
+
+set TestDir="＜PredictLSTMをインストールした場所＞"
