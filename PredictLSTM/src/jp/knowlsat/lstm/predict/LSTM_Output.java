e0372e027eeffe08acc12e92eb20c22b2406722a
tanaka <tanaka@knowlsat.jp>
2024-02-27 23:37:54 +0900

    出力インターフェースに関する不具合修正
    1. 出力項目から実測値を除外（コメントアウト）
    　 1-1. 通常運用時は未来時刻における予測を行うため実測値は取得不可
       1-2. 将来的には過去からの再予測を行った際に統計情報も合わせてを出力することを想定
       （1-2. はコメントアウトとした理由）
    
    2. 不要なimport文、及び、Exception行（FileAlreadyExistsException）を削除

--- PredictLSTM/src/jp/knowlsat/lstm/predict/LSTM_Output.java
+++ PredictLSTM/src/jp/knowlsat/lstm/predict/LSTM_Output.java
@@ -3,7 +3,6 @@ package jp.knowlsat.lstm.predict;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -14,18 +13,9 @@ public class LSTM_Output {
 	public static final String STATISTICS_PATH = "output\\statistics";
 
 	public String z_coDatetime; // 定刻日時
-	public String z_datetime; // 日時
-	public boolean z_flag; // 取水フラグ
-	public boolean onJudge; // 取水判断
 	public double inv_p; // 予測値
-	public double inv_t; // 実測値
-	public double e; // 誤差
-	public double PredictSquaredError; // 二乗誤差
-	public double per; // %誤差
-	public double e2; // 補正誤差
-	public double PredictSquaredError2; // 補正二乗誤差
-	public double per2; // 補正%誤差
 	public double next; // 正規化予測値
+	public boolean onJudge; // 取水判断
 	public boolean incident; // インシデント
 	public String minute; // タイムモード
 	public double[] input; // 入力データ[dataType]
@@ -38,25 +28,36 @@ public class LSTM_Output {
 	public Path input_path;
 	public Path statistics_path;
 
+	//	public String z_datetime; // 日時
+	//	public double inv_t; // 実測値
+	//	public boolean z_flag; // 取水フラグ
+	//	public double e; // 誤差
+	//	public double PredictSquaredError; // 二乗誤差
+	//	public double per; // %誤差
+	//	public double e2; // 補正誤差
+	//	public double PredictSquaredError2; // 補正二乗誤差
+	//	public double per2; // 補正%誤差
+
 	public LSTM_Output(String z_coDatetime, String z_datetime, boolean onJudge, boolean z_flag, double inv_p,
 			double inv_t, double e, double PredictSquaredError, double per, double e2, double PredictSquaredError2,
 			double per2, double next, boolean incident, String minute, double[] input) {
 		this.z_coDatetime = z_coDatetime;
-		this.z_datetime = z_datetime;
-		this.onJudge = onJudge;
-		this.z_flag = z_flag;
 		this.inv_p = inv_p;
-		this.inv_t = inv_t;
-		this.e = e;
-		this.PredictSquaredError = PredictSquaredError;
-		this.per = per;
-		this.e2 = e2;
-		this.PredictSquaredError2 = PredictSquaredError2;
-		this.per2 = per2;
 		this.next = next;
+		this.onJudge = onJudge;
 		this.incident = incident;
 		this.minute = minute;
 		this.input = input;
+
+		// this.z_datetime = z_datetime;
+		// this.inv_t = inv_t;
+		// this.z_flag = z_flag;
+		// this.e = e;
+		// this.PredictSquaredError = PredictSquaredError;
+		// this.per = per;
+		// this.e2 = e2;
+		// this.PredictSquaredError2 = PredictSquaredError2;
+		// this.per2 = per2;
 
 		setInputPath();
 	}
@@ -74,7 +75,6 @@ public class LSTM_Output {
 			Files.createDirectories(Path.of(STATISTICS_PATH + "\\" + year + "\\" + month));
 			Files.createDirectories(Path.of(INPUT_PATH + "\\" + year + "\\" + month + "\\" + day));
 			Files.createDirectories(Path.of(STATISTICS_PATH + "\\" + year + "\\" + month + "\\" + day));
-		} catch (FileAlreadyExistsException e) {
 		} catch (Exception e) {
 			System.out.println("Failed to create error log directory. " + e.getMessage());
 			System.exit(-1);
@@ -168,11 +168,7 @@ public class LSTM_Output {
 			fileWriter.write(",");
 			fileWriter.write("inv_p");
 			fileWriter.write(",");
-			fileWriter.write("inv_t");
-			fileWriter.write(",");
 			fileWriter.write("onJudge");
-			fileWriter.write(",");
-			fileWriter.write("z_flag");
 			fileWriter.write(",");
 
 			for (int i = 0; i < input.length; i++) {
@@ -181,17 +177,29 @@ public class LSTM_Output {
 			}
 
 			fileWriter.write("incident");
+			// fileWriter.write(",");
+			// fileWriter.write("inv_t");
+			// fileWriter.write(",");
+			// fileWriter.write("z_flag");
+			// fileWriter.write(",");
+			// fileWriter.write("e");
+			// fileWriter.write(",");
+			// fileWriter.write("PredictSquaredError");
+			// fileWriter.write(",");
+			// fileWriter.write("per");
+			// fileWriter.write(",");
+			// fileWriter.write("e2");
+			// fileWriter.write(",");
+			// fileWriter.write("PredictSquaredError2");
+			// fileWriter.write(",");
+			// fileWriter.write("per2");
 			fileWriter.write("\n");
 
 			fileWriter.write(z_coDatetime);
 			fileWriter.write(",");
 			fileWriter.write(Double.toString(inv_p));
 			fileWriter.write(",");
-			fileWriter.write(Double.toString(inv_t));
-			fileWriter.write(",");
 			fileWriter.write(Boolean.toString(onJudge));
-			fileWriter.write(",");
-			fileWriter.write(Boolean.toString(z_flag));
 			fileWriter.write(",");
 
 			for (int i = 0; i < input.length; i++) {
@@ -200,6 +208,22 @@ public class LSTM_Output {
 			}
 
 			fileWriter.write(Boolean.toString(incident));
+			// fileWriter.write(",");
+			// fileWriter.write(Double.toString(inv_t));
+			// fileWriter.write(",");
+			// fileWriter.write(Boolean.toString(z_flag));
+			// fileWriter.write(",");
+			// fileWriter.write(Double.toString(e));
+			// fileWriter.write(",");
+			// fileWriter.write(Double.toString(PredictSquaredError));
+			// fileWriter.write(",");
+			// fileWriter.write(Double.toString(per));
+			// fileWriter.write(",");
+			// fileWriter.write(Double.toString(e2));
+			// fileWriter.write(",");
+			// fileWriter.write(Double.toString(PredictSquaredError2));
+			// fileWriter.write(",");
+			// fileWriter.write(Double.toString(per2));
 			fileWriter.write("\n");
 			fileWriter.flush();
 		} catch (IOException e) {
